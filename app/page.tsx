392d78641c0ad06dd611ebaa5993005113e3bbc9
Ezimoha Chigemezu .H <chigemezuezimoha@gmail.com>
2026-05-03 08:47:15 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 // import { useState } from "react";
 // import { Card, CardContent } from "@/components/ui/card";
 // import { Button } from "@/components/ui/button";
@@ -54,7 +54,7 @@
 //     </div>
 //   );
 // }
-
+"use client";
 import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
